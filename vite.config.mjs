51a006472dfc87439e2c47bb67cd9ce31a811bad
caagedal <caagedal@gmail.com>
2025-04-20 22:47:00 +0200

    aaaaaaa

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'user/login/index.html'),
+        listing: resolve(__dirname, 'listing/index.html'),
+        // legg til flere sider her om du har dem
+      }
+    }
+  }
 })
